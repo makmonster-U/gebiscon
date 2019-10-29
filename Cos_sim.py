cd0ad9088e9d0a92ff9a77d48dc6f58cb8492efb
MingiKim <makmonster@naver.com>
2019-10-29 20:44:10 +0900

    행 삭제 추가

--- Cos_sim.py
+++ Cos_sim.py
@@ -1,18 +1,24 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv("test_data/data_created.csv")    #원본파일
-df1 = pd.read_csv("test_data/data_ori2.csv")   #비식별파일
+df = pd.read_csv("test_data/data_ori2.csv")    #원본파일
+df1 = pd.read_csv("test_data/data_created.csv")   #비식별파일
 
 #일련번호순으로 정렬
 s_df = df.sort_values(by='일련번호') #index는 일련번호의미
 s_df1 = df1.sort_values(by='일련번호')
+
+
+#삭제된 행 처리
+deleted_rows = list(set(s_df['일련번호']) - set(s_df1['일련번호']))
+s_df.drop(deleted_rows)
 
 #컬럼지정
 interest_column = pd.Series(['신용대출한도','월소득']) #원하는 열 선택
 
 
 #컬럼별 연산
+
 cos = {}
 for i in interest_column:
     dot = np.dot(df[i], df1[i])
