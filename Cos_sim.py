eaf527ea4aef61e21e4b74b789853c9ccc5bd17d
MingiKim <makmonster@naver.com>
2019-10-30 03:30:08 +0900

    수정

--- Cos_sim.py
+++ Cos_sim.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_csv("test_data/data_ori2.csv")    #원본파일
-df1 = pd.read_csv("test_data/data_created.csv")   #비식별파일
+df1 = pd.read_csv("test_data/data_ori2.csv")   #비식별파일
 
 #일련번호순으로 정렬
 s_df = df.sort_values(by='일련번호') #index는 일련번호의미
@@ -20,14 +20,15 @@ count = -1
 deleted_index=[]
 k = len(deleted_rows)
     #삭제된 행은 원본에서도 삭제
-for i in s_df['일련번호']:
-    count = count + 1
-    if i == deleted_rows[j]:
-        deleted_index.append(count)
-        j = j + 1
-        if j>k:
-            break
-        continue
+if deleted_rows:
+    for i in s_df['일련번호']:
+        count = count + 1
+        if i == deleted_rows[j]:
+            deleted_index.append(count)
+            j = j + 1
+            if j>k:
+                break
+            continue
 s_df = s_df.drop(deleted_index)
 
 #컬럼별 연산
